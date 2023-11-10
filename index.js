cf2db3a4413a2bb19d9a76ad2aa0ec836a1f4b36
Sabeeh098 <sabeehup@gmail.com>
2023-11-10 09:53:38 +0530

    Cors add *

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors('*'));
 
 app.use(express.json({ limit: "100mb", extended: true }));
 app.use(bodyParser.json());
