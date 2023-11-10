f63726b78ce2b0103f4f17533e33997e263463c6
Sabeeh098 <sabeehup@gmail.com>
2023-11-10 14:54:09 +0530

    done

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.CLIENTURL,
+    origin: '*',
   },
 });
 io.on("connection", (socket) => {
