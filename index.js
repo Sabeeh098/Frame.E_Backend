2025c2b108847c2c83c3c40f439158c842b8918f
Sabeeh098 <sabeehup@gmail.com>
2023-11-10 11:27:04 +0530

    Url Changed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.FRONTENDURL,
+    origin: process.env.CLIENTURL,
   },
 });
 io.on("connection", (socket) => {
